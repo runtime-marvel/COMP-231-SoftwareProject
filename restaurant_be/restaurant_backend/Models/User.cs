edecef478e7fc5b0097f1a0b497814084ef213c8
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-07-20 15:51:09 -0400

    Authentication base implementation completed

---
+++ restaurant_be/restaurant_backend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace restaurant_backend.Models
+{
+    public class User
+    {
+
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
---
+++ restaurant_be/restaurant_backend/Models/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace restaurant_backend.Models
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set;} = string.Empty;
+
+    }
+}
