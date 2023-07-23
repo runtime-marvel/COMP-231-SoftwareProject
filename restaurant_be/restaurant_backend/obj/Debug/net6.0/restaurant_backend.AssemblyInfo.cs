27becbd40674977d4266ec72408a48f8a764aa58
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-07-23 16:50:41 -0400

    Added Docker support for hosting on Render

--- restaurant_be/restaurant_backend/obj/Debug/net6.0/restaurant_backend.AssemblyInfo.cs
+++ restaurant_be/restaurant_backend/obj/Debug/net6.0/restaurant_backend.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0c9a9bcb-a290-40a3-b503-6624464ef353")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
